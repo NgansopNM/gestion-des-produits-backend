605e559f11fda91774a9bd91528024d775ae9e71
NgansopNM <martialngansop4@gmail.com>
2025-12-28 12:32:08 +0100

    Ajout du render.yaml

--- API-produit/src/main/java/com/martialdev/API_produit/Config/CorsConfig.java
+++ API-produit/src/main/java/com/martialdev/API_produit/Config/CorsConfig.java
@@ -9,8 +9,10 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("http://localhost:5173",
+                        "https://*.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*");
+                .allowedHeaders("*")
+                .allowCredentials(false);
     }
 }
--- API-produit/src/main/java/com/martialdev/API_produit/Controller/ProductController.java
+++ API-produit/src/main/java/com/martialdev/API_produit/Controller/ProductController.java
@@ -5,14 +5,13 @@ import com.martialdev.API_produit.Dto.ProductRequestDTO;
 import com.martialdev.API_produit.Dto.ProductResponseDTO;
 import com.martialdev.API_produit.Service.ProductService;
 import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/products")
-@CrossOrigin(origins = "http://localhost:5173") // React
+@CrossOrigin(origins = {"http://localhost:5173", "https://ton-frontend.vercel.app"}) // React
 public class ProductController {
 
     private final ProductService service;
